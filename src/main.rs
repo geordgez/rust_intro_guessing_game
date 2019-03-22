213cc0e1e60c47ee40946fa1050b44f2b5d75725
George Zeng <georgejzeng@gmail.com>
2019-03-22 15:50:31 -0400

    Loop until user guesses correct number

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,29 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 5);
 
     println!("The secret number is: {}", secret_number);
+    
+    loop {
 
-    println!("Guess the number!\nPlease input your guess.");
+        println!("Guess the number!\nPlease input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small... guess something bigger"),
-        Ordering::Equal => println!("You got it! The correct number was {}", secret_number),
-        Ordering::Greater => println!("Too big... guess something smaller"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small... guess something bigger"),
+            Ordering::Equal => {
+                println!("You got it! The correct number was {}", secret_number);
+                break;
+            },
+            Ordering::Greater => println!("Too big... guess something smaller"),
+        }
+
     }
 }
